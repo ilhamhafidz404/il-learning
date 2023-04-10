e3691b569b218a6a3f4763a8b554992cee779066
ilhamhafidz404 <xxspanzie@gmail.com>
2023-04-10 14:05:50 +0700

    Persiapan fix bug, progress bar menyatu dengan classroom lain

--- database/migrations/2023_01_16_023456_create_missions_table.php
+++ database/migrations/2023_01_16_023456_create_missions_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug');
             $table->foreignId('course_id');
+            $table->foreignId('classroom_id');
             $table->timestamps();
         });
     }
