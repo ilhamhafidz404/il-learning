89fef762defe87136b6a237c44eb7fd1afb40439
ilhamhafidz404 <xxspanzie@gmail.com>
2023-04-11 11:05:22 +0700

    Fix count / total submission pada keterangan mission

--- resources/views/backend/oneForAll/course/show.blade.php
+++ resources/views/backend/oneForAll/course/show.blade.php
@@ -131,7 +131,16 @@
                                     >
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                                     </svg>
-                                    Has {{ $mission->submission->count() }} submission
+                                    Has 
+                                    @if (Auth::user()->hasRole('student'))
+                                        {{ $mission->submission[0]
+                                            ->whereClassroomId(Auth::user()->student[0]->classroom_id)
+                                            ->count() 
+                                        }} 
+                                    @else
+                                        {{ $mission->submission->count() }} 
+                                    @endif
+                                    submission
                                 </small>
                             </div>
                             <h4 class="font-bold">
