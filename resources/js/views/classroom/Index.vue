3d45af67c9c8b8d0bc2c6f9241bc5a5b6f763774
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2023-09-07 20:52:58 +0700

    fix: View List Card Classroom (IL-9)

--- resources/js/views/classroom/Index.vue
+++ resources/js/views/classroom/Index.vue
@@ -1,28 +1,44 @@
 <template>
-  <DashboardLayout>
-    <section class="mt-24 col-span-4 pr-7">
-      <div class="card bg-primary shadow-xl">
-        <div class="card-body">
-          <div class="flex justify-between items-center w-full">
-            <div>
-              <h2 class="card-tittle font-extrabold font-3xl">TNFC-2020-8-3</h2>
-              <p class="font-thin text-sm">Maman Tegar S.kom M.kom</p>
+    <DashboardLayout>
+        <section class="mt-24 grid col-span-2 pr-7">
+            <div class="card bg-darkgrey shadow-2xl">
+                <div class="card-body">
+                    <div class="flex justify-between items-center w-full">
+                        <div>
+                            <h2 class="card-tittle font-extrabold font-3xl">TNFC-2020-8-3</h2>
+                            <p class="font-thin text-sm">Maman Tegar S.kom M.kom</p>
+                        </div>
+                        <span class="indicator-item indicator-middle indicator-end badge badge-primary">3</span>
+                    </div>
+                </div>
             </div>
-            <p class="text-end">3</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  </DashboardLayout>
+        </section>
+        <section class="mt-24 grid col-span-2 pr-7">
+            <div class="card darkgrey shadow-2xl">
+                <div class="card-body">
+                    <div class="flex justify-between items-center w-full">
+                        <div>
+                            <h2 class="card-tittle font-extrabold font-3xl">TNFC-2020-8-3</h2>
+                            <p class="font-thin text-sm">Maman Tegar S.kom M.kom</p>
+                        </div>
+                        <span class="indicator-item indicator-middle indicator-end badge badge-primary">13</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </DashboardLayout>
 </template>
 
 <script>
-import DashboardLayout from "../DashboardLayout.vue";
-export default {
-  components: {
-    DashboardLayout,
-  },
-};
+    import DashboardLayout from "../DashboardLayout.vue";
+
+    //actions
+    import {getClassrooms} from "../../api/Classroom";
+    export default {
+        components: {
+            DashboardLayout
+        },
+    };
 </script>
 
 <style></style>
