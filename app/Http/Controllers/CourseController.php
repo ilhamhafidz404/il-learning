4c2018022f64bc815554c6be5bdebde6ec8de734
ilhamhafidz404 <xxspanzie@gmail.com>
2023-09-17 21:57:08 +0700

    feat: Page show course for lecturer (IL-24)

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -10,10 +10,10 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api');
+    // }
 
     public function index()
     {
