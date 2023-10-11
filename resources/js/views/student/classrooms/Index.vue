65e6a2bc0fbc37f9769ef0b170442e74cd5652c2
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2023-10-11 20:40:38 +0700

    fix get classroom

--- resources/js/views/student/classrooms/Index.vue
+++ resources/js/views/student/classrooms/Index.vue
@@ -9,12 +9,12 @@
         <div class="card-body">
           <div class="flex justify-between items-center w-full">
             <div>
-              <h2 class="card-tittle font-extrabold font-3xl">TNFC-2020-8-3</h2>
-              <p class="font-thin text-sm">Maman Tegar S.kom M.kom</p>
+              <h2 class="card-tittle font-extrabold font-3xl">{{ classroom.name }}</h2>
+              <p class="font-thin text-sm">{{ classroom.mentor }}</p>
             </div>
             <span
               class="indicator-item indicator-middle indicator-end badge badge-primary"
-              >3</span
+              >{{ classroom.program_id }}</span
             >
           </div>
         </div>
