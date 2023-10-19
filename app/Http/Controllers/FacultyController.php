547ea06dbcd335da4a3f92163eb4364e8454180f
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2023-10-19 22:37:26 +0700

    API: List Categories Courses

---
+++ app/Http/Controllers/FacultyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Faculty;
+use Illuminate\Http\Request;
+
+class FacultyController extends Controller
+{
+    public function index()
+    {
+        $faculties = Faculty::all();
+        return response()->json($faculties);
+    }   
+}
